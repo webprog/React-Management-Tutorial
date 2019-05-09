b5f074ae86cdde95416faab3c6d0a97cc83b6618
webprog <webprog@nate.com>
2019-05-10 01:36:43 +0900

    .

--- src/App.js
+++ src/App.js
@@ -2,16 +2,43 @@ import React from 'react';
 import Customer from './components/Customer';
 import './App.css';
 
-const customer = {
-  'name' : '홍길동',
-  'birthday' : '910222',
-  'gender' : '남자',
-  'job' : '대학생'
-};
+const customers = [
+    {
+      'id' : 1,
+      'image' : 'https://placeimg.com/64/64/1',
+      'name' : '홍길동',
+      'birthday' : '910222',
+      'gender' : '남자',
+      'job' : '대학생'
+    },
+    {
+      'id' : 2,
+      'image' : 'https://placeimg.com/64/64/2',
+      'name' : '홍길동',
+      'birthday' : '910222',
+      'gender' : '남자',
+      'job' : '대학생'
+    },
+    {
+      'id' : 3,
+      'image' : 'https://placeimg.com/64/64/3',
+      'name' : '홍길동',
+      'birthday' : '910222',
+      'gender' : '남자',
+      'job' : '대학생'
+    }
+];
 
 function App() {
   return (
-    <Customer data={customer}/>
+    {
+      customers.map(c => {
+        return (
+          <Customer data={customer}/>
+        );
+      })
+    }
+    
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,12 +4,32 @@ class Customer extends React.Component {
     render() {
         return (
             <div>
-                <h2>{this.props.data.name}</h2>
-                <p>{this.props.data.birthday}</p>
-                <p>{this.props.data.gender}</p>
-                <p>{this.props.data.job}</p>
+                <CustomerProfile data={this.props}/>
+                <CustomerInfo data={this.props}/>
             </div>
         );
+    }
+}
+
+class CustomerProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.data.image} alt="profile" />
+                <h2>{this.props.data.name}({this.props.data.id})</h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.data.birthday}</p>
+                <p>{this.props.data.job}</p>
+            </div>
+        )
     }
 }
 
